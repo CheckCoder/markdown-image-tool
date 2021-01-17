62d834e640fc6f272fa0730035914b9ca286a168
CheckCoder <checkWork@126.com>
2021-01-17 20:25:02 +0800

    chore: 完善 qiniu 上传错误拦截

--- utils/cloud.js
+++ utils/cloud.js
@@ -8,39 +8,47 @@ const qiniu = require('qiniu');
  */
 function uploadFile(config, localPath, cloudFileName) {
     return new Promise((resolve, reject) => {
-        if (config.cloud === 'qiniu') {
-            let { accessKey, secretKey, bucket, urlPrefix, urlSuffix, zone } = config.qiniu;
-
-            let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-            let options = {
-                scope: bucket,
-            };
-            let putPolicy = new qiniu.rs.PutPolicy(options);
-            let uploadToken = putPolicy.uploadToken(mac);
-        
-            let qiniuConfig = new qiniu.conf.Config();
-            if (zone) {
-                qiniuConfig.zone = qiniu.zone[zone];
-            }
-            let formUploader = new qiniu.form_up.FormUploader(qiniuConfig);
-            let putExtra = new qiniu.form_up.PutExtra();
-        
-            formUploader.putFile(uploadToken, cloudFileName, localPath, putExtra, (respErr, respBody, respInfo) => {
-                if (respErr) {
-                    reject({
-                        msg: 'qiniu 上传文件失败',
-                        err: respErr
-                    });
-                } else if (respInfo.statusCode == 200) {
-                    resolve(`${urlPrefix}${cloudFileName}${urlSuffix}`);
-                } else {
-                    reject({
-                        msg: 'qiniu 上传文件失败',
-                        err: respBody
-                    });
+        try {
+            if (config.cloud === 'qiniu') {
+                let { accessKey, secretKey, bucket, urlPrefix, urlSuffix, zone } = config.qiniu;
+    
+                let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+                let options = {
+                    scope: bucket,
+                };
+                let putPolicy = new qiniu.rs.PutPolicy(options);
+                let uploadToken = putPolicy.uploadToken(mac);
+            
+                let qiniuConfig = new qiniu.conf.Config();
+                if (zone) {
+                    qiniuConfig.zone = qiniu.zone[zone];
                 }
-            });
+                let formUploader = new qiniu.form_up.FormUploader(qiniuConfig);
+                let putExtra = new qiniu.form_up.PutExtra();
+            
+                formUploader.putFile(uploadToken, cloudFileName, localPath, putExtra, (respErr, respBody, respInfo) => {
+                    if (respErr) {
+                        reject({
+                            msg: 'qiniu 上传文件失败',
+                            err: respErr
+                        });
+                    } else if (respInfo.statusCode == 200) {
+                        resolve(`${urlPrefix}${cloudFileName}${urlSuffix}`);
+                    } else {
+                        reject({
+                            msg: 'qiniu 上传文件失败',
+                            err: respBody
+                        });
+                    }
+                });
+            }
+        } catch (err) {
+            reject({
+                msg: 'qiniu 上传文件失败',
+                err
+            })
         }
+
     });
 }
 
